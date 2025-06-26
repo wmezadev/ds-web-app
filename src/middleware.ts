50ee078cf985a65a95d6a041d9e20418f5f49d30
wmezadev <5531063+wmezadev@users.noreply.github.com>
2025-06-26 11:35:19 -0500

    fix: Middleware static image generation error

--- src/middleware.ts
+++ src/middleware.ts
@@ -17,6 +17,6 @@ export default withAuth(
 export const config = {
   matcher: [
     // Protect all routes except auth-related ones
-    '/((?!api/auth|login|_next/static|_next/image|favicon.ico).*)'
+    '/((?!api/auth|login|_next/static|_next/image|images|favicon.ico).*)'
   ]
 }
