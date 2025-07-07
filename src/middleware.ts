82b788edb0bce6dba436dfa6965165dd926a2ac8
wmezadev <5531063+wmezadev@users.noreply.github.com>
2025-07-07 11:56:35 -0500

    fix: Remove unused console logs

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,25 +3,13 @@ import { NextResponse } from 'next/server'
 import { withAuth } from 'next-auth/middleware'
 
 export default withAuth(
-  function middleware(req) {
-    // Debug: log token and pathname
-    // @ts-ignore
-    console.log('MIDDLEWARE:', {
-      pathname: req.nextUrl.pathname,
-      token: req.nextauth?.token
-    })
-
+  function middleware() {
+    // custom logic here
     return NextResponse.next()
   },
   {
     callbacks: {
-      authorized: ({ token }) => {
-        // Debug: log token in callback
-        // @ts-ignore
-        console.log('AUTHORIZED CALLBACK:', { token })
-
-        return !!token
-      }
+      authorized: ({ token }) => !!token
     }
   }
 )
