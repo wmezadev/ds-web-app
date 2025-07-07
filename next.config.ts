556940609c771187c149ded17c0b8f5934c68cda
wmezadev <5531063+wmezadev@users.noreply.github.com>
2025-07-07 11:22:18 -0500

    debug: expose auth secret

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,9 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  env: {
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET
+  }
+}
 
 export default nextConfig
